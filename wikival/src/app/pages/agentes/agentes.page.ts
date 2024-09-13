212209f9d144048da90202ce21aec3e5dafcd115
benjamnunez <133883580+benjamnunez@users.noreply.github.com>
2024-09-13 13:32:44 -0300

    nueva app

---
+++ wikival/src/app/pages/agentes/agentes.page.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { ValorantapiService } from 'src/app/services/valorantapi.service';
+//import {  } from "../";
+
+
+@Component({
+  selector: 'app-agentes',
+  templateUrl: './agentes.page.html',
+  styleUrls: ['./agentes.page.scss'],
+})
+export class AgentesPage implements OnInit {
+agents: any[]=[];
+  
+  constructor(private valorantService: ValorantapiService) { }
+
+  ngOnInit() {
+
+  }
+  mostrarAgents(){
+    this.valorantService.mostrarAgentes().subscribe((data) =>{
+      this.agents=data.data;
+      console.log(this.agents)
+    } , 
+    (error)=>{
+      console.error('Error al obtener agentes:', error)
+    }
+    );
+  }
+
+}
---
+++ wikival/src/app/services/valorantapi.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ValorantapiService {
+  private apiUrl: string='https://valorant-api.com';
+  public lenguaje: string='language=es-MX';
+  public apiAgentes: string='/v1/agents?';
+  constructor(private http: HttpClient) { }
+  mostrarAgentes(): Observable<any>{
+    console.log('Requesting:', this.apiUrl+this.apiAgentes+this.lenguaje); // Verificar la URL solicitada
+    return this.http.get(this.apiUrl+this.apiAgentes+this.lenguaje);
+  }
+}
