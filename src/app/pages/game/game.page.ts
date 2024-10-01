0bf9d5c6dcd11d6dfb679ea6e94d40b650030dfe
benjamnunez <133883580+benjamnunez@users.noreply.github.com>
2024-09-30 22:41:35 -0300

    cambios minijuego

--- src/app/pages/game/game.page.ts
+++ src/app/pages/game/game.page.ts
@@ -21,7 +21,13 @@ export class GamePage implements OnInit {
     {name : 'deadlock' , imgUrl : '../../../assets/gameImg/deadlock.png'},
     {name : 'gekko' , imgUrl : '../../../assets/gameImg/gekko.png'},
     {name : 'cypher' , imgUrl : '../../../assets/gameImg/cypher.png'},
-    
+    {name : 'astra' , imgUrl : '../../../assets/gameImg/astra.png'},
+    {name : 'harbor' , imgUrl : '../../../assets/gameImg/harbor.png'},
+    {name : 'killjoy' , imgUrl : '../../../assets/gameImg/kj.png'},
+    {name : 'phoenix' , imgUrl : '../../../assets/gameImg/phoenix.png'},
+    {name : 'sova' , imgUrl : '../../../assets/gameImg/sova.png'},
+    {name : 'vyper' , imgUrl : '../../../assets/gameImg/vyper.png'},
+    {name : 'vyse' , imgUrl : '../../../assets/gameImg/vyse.png'},
   ];
 
   pjRandom: any={name: '', imgUrl: ''};
@@ -47,18 +53,22 @@ export class GamePage implements OnInit {
   msg: string="";
   intentos: number=5;
   isButtonDisabled: boolean = false;
+  racha: number=0;
 
   enviarDatosJuego(){
     if (this.pjRandom.name == this.nombrepj) {
       this.msg="CORRECTO";
       this.generarPersonajeRandom();
       this.intentos=5;
+      this.racha++;
     } else {
       this.msg="ERROR";
       this.intentos--;
+      this.racha=0;
       if (this.intentos===0) {
         this.msg="PERDISTE";
         this.isButtonDisabled = true;
+        
       }
     } 
     
