b908f7c0e1980050ad78775e40fb8336a571991a
benjamnunez <133883580+benjamnunez@users.noreply.github.com>
2024-10-22 00:12:45 -0300

    subida de archivos2

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+
+  firebaseConfig: {
+    apiKey: "AIzaSyD-ffrM7qqsZ10eV0oO-DnxYgRR7DrFLjw",
+    authDomain: "wikival-698f6.firebaseapp.com",
+    projectId: "wikival-698f6",
+    storageBucket: "wikival-698f6.appspot.com",
+    messagingSenderId: "862466345177",
+    appId: "1:862466345177:web:e62fd604724f48d265d6c8"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,19 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+
+  firebaseConfig: {
+    apiKey: "AIzaSyD-ffrM7qqsZ10eV0oO-DnxYgRR7DrFLjw",
+    authDomain: "wikival-698f6.firebaseapp.com",
+    projectId: "wikival-698f6",
+    storageBucket: "wikival-698f6.appspot.com",
+    messagingSenderId: "862466345177",
+    appId: "1:862466345177:web:e62fd604724f48d265d6c8"
+  }
 };
+
+
 
 /*
  * For easier debugging in development mode, you can import the following file
