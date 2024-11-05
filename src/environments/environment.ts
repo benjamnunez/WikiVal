1b8f1fd94bb8d24414a88c99a09b30fb154e93da
Nicolás Andrés González González <133883532+nicolasAG2@users.noreply.github.com>
2024-11-04 22:04:22 -0300

    unión proyecto

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,19 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+
+  firebaseConfig: {
+    apiKey: "AIzaSyDlFyleDNCeMloawiGchVvGaA_K0DD22F8",
+    authDomain: "wikivalo-70ae7.firebaseapp.com",
+    projectId: "wikivalo-70ae7",
+    storageBucket: "wikivalo-70ae7.firebasestorage.app",
+    messagingSenderId: "259157579102",
+    appId: "1:259157579102:web:db348f1802678763fc2bcf"
+  }
 };
+
+
 
 /*
  * For easier debugging in development mode, you can import the following file
