803706ea3c71298936cb55fa790ebdbd19a7ad83
benjamnunez <133883580+benjamnunez@users.noreply.github.com>
2024-12-03 00:28:07 -0300

    Nueva implementacion 2

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,11 +5,11 @@
 export const environment = {
   production: false,
 
-  firebaseConfig: {
+  firebaseConfig : {
     apiKey: "AIzaSyD-ffrM7qqsZ10eV0oO-DnxYgRR7DrFLjw",
     authDomain: "wikival-698f6.firebaseapp.com",
     projectId: "wikival-698f6",
-    storageBucket: "wikival-698f6.appspot.com",
+    storageBucket: "wikival-698f6.firebasestorage.app",
     messagingSenderId: "862466345177",
     appId: "1:862466345177:web:e62fd604724f48d265d6c8"
   }
